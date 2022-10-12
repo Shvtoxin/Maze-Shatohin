008b70039785e194836bfdf53ee0e6645e34950d
Shvtoxin <Sasha.shvtoxin@mail.ru>
2022-10-12 12:17:30 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Лабиринт_Шатохин
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Maze currentMaze = new Maze();
+            currentMaze.Start();
+        }
+    }
+
+}
---
+++ Игрок.cs
@@ -0,0 +1,28 @@
+﻿using static System.Console;
+
+namespace Лабиринт_Шатохин
+{
+
+    internal class Player
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        private string PlayerMarker;
+        private ConsoleColor PlayerColor;
+
+        public Player(int initialX, int initialY )
+        {
+            X = initialX;
+            Y = initialY;
+            PlayerMarker = "O";
+            PlayerColor = ConsoleColor.Red;
+        }
+        public void Draw()
+        {
+            ForegroundColor = PlayerColor;
+            SetCursorPosition( X, Y );
+            Write(PlayerMarker);
+            ResetColor();
+        }
+    }
+}
